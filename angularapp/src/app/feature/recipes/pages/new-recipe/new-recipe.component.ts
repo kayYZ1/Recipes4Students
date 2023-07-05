32f42a832ad999860f71bec21b7cf7ccfb06eda5
kaYzz <106306274+kayYZ1@users.noreply.github.com>
2023-07-05 17:58:56 +0200

    feat: recipe and image url's validation

--- angularapp/src/app/feature/recipes/pages/new-recipe/new-recipe.component.ts
+++ angularapp/src/app/feature/recipes/pages/new-recipe/new-recipe.component.ts
@@ -3,26 +3,28 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 import * as fromAuth from "../../../../core/state/auth/auth.state"
-import { Router } from '@angular/router';
+import { isImageUrl, isRecipeUrlValid } from 'src/app/shared/utils/new-recipe-utils';
 
 @Component({
   selector: 'app-new-recipe',
   templateUrl: './new-recipe.component.html',
   styleUrls: ['./new-recipe.component.css'],
 })
+
 export class NewRecipeComponent {
-  constructor(private _store: Store<fromAuth.State>, private router: Router) {}
-  buttonDisabled = true  
+  constructor(private _store: Store<fromAuth.State>) { }
+  buttonDisabled = true
 
   newRecipeForm = new FormGroup({
     title: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required),
-    imageURL: new FormControl('', Validators.required),
-    recipeURL: new FormControl('', Validators.required),
+    imageURL: new FormControl('',
+      [Validators.required, isImageUrl()]),
+    recipeURL: new FormControl('', [Validators.required, isRecipeUrlValid()]),
   });
 
   user$ = this._store.select(fromAuth.selectUser)
-  
+
   ngOnInit() {
     this.newRecipeForm.valueChanges.subscribe(() => {
       this.buttonDisabled = this.newRecipeForm.invalid
