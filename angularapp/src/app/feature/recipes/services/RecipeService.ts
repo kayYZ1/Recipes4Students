4aff109734fbe599575f3ae2f681a3c1a02c8d6e
kaYzz <106306274+kayYZ1@users.noreply.github.com>
2023-07-06 12:03:05 +0200

    feat: async pipe in new-recipe template

--- angularapp/src/app/feature/recipes/services/RecipeService.ts
+++ angularapp/src/app/feature/recipes/services/RecipeService.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core"
+import { Recipe } from "src/app/core/models/Recipe";
 
 @Injectable({
   providedIn: "root"
@@ -10,5 +11,9 @@ export class RecipeService {
 
   fetchData() {
     return this._http.get("http://localhost:5200/api/Recipes")
+  }
+
+  addRecipe(values: Recipe) {
+    return this._http.post("http://localhost:5200/api/Recipes", values)
   }
 }
